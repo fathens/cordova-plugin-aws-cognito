c54874e0f94cb34c913308718f6706223a5f322f
Kunio Sawatani <devel@fathens.org>
2017-01-06 10:52:29 +0900

    www/cognito.js を作成

---
+++ www/cognito.js
@@ -0,0 +1,22 @@
+var cordova = require("cordova/exec");
+
+var pluginName = "AWSCognitoPlugin"
+
+var names = [
+"getIdentity",
+"setToken",
+"removeToken"
+];
+
+var obj = {};
+
+names.forEach(function(methodName) {
+    obj[methodName] = function() {
+        var args = Array.prototype.slice.call(arguments, 0);
+        return new Promise(function(resolve, reject) {
+            cordova(resolve, reject, pluginName, methodName, args);
+        });
+    }
+});
+
+module.exports = obj;
